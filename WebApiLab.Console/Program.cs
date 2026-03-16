95d64700060cdc69cd83db9c540fbd104e0395f4
Austin <auhauger@gmail.com>
2026-03-15 20:53:38 -0400

    console app

---
+++ WebApiLab.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using WebApiLab.Console.Models;
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri("http://localhost:5115");
+
+HttpResponseMessage response = await client.GetAsync("/people");
+
+if (response.IsSuccessStatusCode)
+{
+    string jsonResponse = await response.Content.ReadAsStringAsync();
+
+    var people = JsonSerializer.Deserialize<List<Person>>(
+        jsonResponse,
+        new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+    foreach (var person in people)
+    {
+        Console.WriteLine($"{person.Name} speaks {person.Language}");
+    }
+}
+else
+{
+    Console.WriteLine($"Error: {response.StatusCode}");
+    Console.WriteLine(await response.Content.ReadAsStringAsync());
+}
