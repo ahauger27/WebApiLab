0719fc9ec68561db0f3ce06681fdbc4f44ad3ed6
Austin <auhauger@gmail.com>
2026-03-19 11:20:59 -0400

    lookup functions

--- WebApiLab.Console/Program.cs
+++ WebApiLab.Console/Program.cs
@@ -6,13 +6,16 @@ client.BaseAddress = new Uri("http://localhost:5115");
 
 HttpResponseMessage response = await client.GetAsync("/api/People");
 
+var options = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true
+};
+
 if (response.IsSuccessStatusCode)
 {
     string jsonResponse = await response.Content.ReadAsStringAsync();
 
-    var people = JsonSerializer.Deserialize<List<Person>>(
-        jsonResponse,
-        new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+    var people = JsonSerializer.Deserialize<List<Person>>(jsonResponse, options);
 
     foreach (var person in people)
     {
@@ -23,4 +26,20 @@ else
 {
     Console.WriteLine($"Error: {response.StatusCode}");
     Console.WriteLine(await response.Content.ReadAsStringAsync());
+}
+
+HttpResponseMessage singleResponse = await client.GetAsync("/api/People/V59OF92YF627HFY0");
+
+if (response.IsSuccessStatusCode)
+{
+    string jsonResponse = await singleResponse.Content.ReadAsStringAsync();
+
+    var person = JsonSerializer.Deserialize<Person>(jsonResponse, options);
+
+    Console.WriteLine($"{person.Name} speaks {person.Language}");
+}
+else
+{
+    Console.WriteLine($"Error: {singleResponse.StatusCode}");
+    Console.WriteLine(await singleResponse.Content.ReadAsStringAsync());
 }
--- WebApiLab.Console/obj/Debug/net10.0/WebApiLab.Console.AssemblyInfo.cs
+++ WebApiLab.Console/obj/Debug/net10.0/WebApiLab.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiLab.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95d64700060cdc69cd83db9c540fbd104e0395f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98891f9016b6a06e59d3d9eab04cdfcb074290c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiLab.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiLab.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
